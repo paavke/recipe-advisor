537ca83c891ee580589d680b4ac643b8f3c20671
paavke <127980588+paavke@users.noreply.github.com>
2023-05-31 19:44:00 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "9c35f48a-70f5-44c6-bfef-3bcf9c33f57e";
+export const MODAL_CLOSE_SEC = 2.5;
